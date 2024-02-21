bab24a36357b196934ee0a4e6fb7d9ee498db327
AckimJnr <acklongwe@gmail.com>
2024-02-21 21:34:21 +0200

    Add book access resource

--- app/Http/Controllers/BookAccessController.php
+++ app/Http/Controllers/BookAccessController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class BookAccessController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function show(Request $request, BookAccess $book_access)
+    {
+
+    }
+
+    public function store(StoreBookAccessRequest $request)
+    {
+
+    }
+
+    public function update(UpdateBookAccessRequest $request, BookAccess $book_access)
+    {
+
+    }
 }
